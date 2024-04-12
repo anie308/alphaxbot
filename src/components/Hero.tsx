1c33e66a243bdd071a14661b2e6cd0c93da02d95
Aniebietabasi <aniebietabasiobo@gmail.com>
2024-04-12 12:51:56 +0100

    commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,28 +2,37 @@ import Image from "next/image";
 import React from "react";
 import herobg from "@/assets/herobg.svg";
 import time from "@/assets/time.svg";
-
+import { CiCircleCheck } from "react-icons/ci";
 import { FaArrowRightLong } from "react-icons/fa6";
 
 function Hero() {
   return (
-    <div className="overflow-hidden h-[600px] lg:h-[650px] relative">
-      <Image src={herobg} alt="hero" className="w-full h-full object-cover" />
-      <div className="absolute top-0 w-full h-full grid lg:grid-cols-2 gap-[20px] p-[20px] items-center">
-        <div className="mt-[20px] lg:mt-0 lg:pl-[40px]">
-          <p className="text-[35px] lg:text-[64px] font-[600] leading-[40px] lg:leading-[76px] font-syne">
-            Trade like an ALPHA with AI-powered{" "}
-            <span className="text-[#6942E2]">onchain</span> analysis!
-          </p>
-          <button className="rounded-[4px]  p-[10px_40px] mt-[20px] lg:mt-[30px] bg-[#6942E2] text-white font-syne flex items-center space-x-[20px]">
-            <span>Become an Alpha Trader</span> <FaArrowRightLong />
-          </button>
-        </div>
-        <div className="flex items-start lg:items-center justify-center w-full h-full">
-          <Image src={time} alt="" className="h-[60%] lg:h-full" />
+    <>
+      <div className="overflow-hidden h-[600px] lg:h-[650px] relative">
+        <Image src={herobg} alt="hero" className="w-full h-full object-cover" />
+        <div className="absolute top-0 w-full h-full grid lg:grid-cols-2 gap-[20px] p-[20px] items-center">
+          <div className="mt-[20px] lg:mt-0 lg:pl-[40px]">
+            <p className="text-[35px] lg:text-[64px] font-[600] leading-[40px] lg:leading-[76px] font-syne">
+              Trade like an ALPHA with AI-powered
+              <span className="text-[#6942E2]">onchain</span> analysis!
+            </p>
+            <button className="rounded-[4px]  p-[10px_40px] mt-[20px] lg:mt-[30px] bg-[#6942E2] text-white font-syne flex items-center space-x-[20px]">
+              <span>Become an Alpha Trader</span> <FaArrowRightLong />
+            </button>
+          </div>
+          <div className="flex items-start lg:items-center justify-center w-full h-full">
+            <Image src={time} alt="" className="h-[60%] lg:h-full" />
+          </div>
         </div>
       </div>
-    </div>
+      <div className="w-full bottom-[-13%] lg:bottom-[-9%] flex items-center justify-center absolute ">
+        <div className="w-[90%]  lg:w-[70%] flex items-center space-x-[5px] lg:space-x-[15px] justify-center bg-[#D9D9D933] h-[50px] lg:h-[80px] rounded-[20px] p-[10px]">
+        <CiCircleCheck className="text-[25px]" />
+        <span className="text-[14px] lg:text-[20px] font-syne">Tested and trusted by Alphas of the Trading Game...</span>
+        
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/components/RoadMap.tsx
+++ src/components/RoadMap.tsx
@@ -25,11 +25,54 @@ function RoadMap() {
             </p>
             <FaCircleChevronDown className="text-white text-[30px]" />
           </div>
-          <div className="bg-white m-[10px_20px]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
-            assumenda nostrum nam nihil at ullam quos veniam consectetur iusto
-            corrupti, a ipsa laborum sit fugit eligendi placeat, quia error
-            eveniet.
+          <div className="bg-white m-[10px_20px] p-[10px] rounded-[6px] flex flex-col space-y-[10px]">
+            <div>
+              <p className="font-syne text-[18px] font-[600] text-[#081428]">
+                1. Platform Development
+              </p>
+              <ul className="font-syne list-disc px-[20px]">
+                <li>
+                  Develop the core features of AlphaXBot, including signal
+                  generation, buy/sell options, and user interface.
+                </li>
+                <li>
+                  Integrate onchain analysis and AI algorithms for accurate
+                  signal generation.
+                </li>
+                <li>Ensure compatibility with multiple blockchain networks.</li>
+              </ul>
+            </div>
+            <div>
+              <p className="font-syne text-[18px] font-[600] text-[#081428]">
+                2. Beta Testing
+              </p>
+              <ul className="font-syne list-disc px-[20px]">
+                <li>
+                  Conduct rigorous testing of the platform&apos;s functionality,
+                  performance, and security.
+                </li>
+                <li>
+                  Gather feedback from beta testers to identify bugs, usability
+                  issues, and areas for improvement.
+                </li>
+                <li>Iterate on the platform based on beta test results.</li>
+              </ul>
+            </div>
+            <div>
+              <p className="font-syne text-[18px] font-[600] text-[#081428]">
+                3. Community Building
+              </p>
+              <ul className="font-syne list-disc px-[20px]">
+                <li>
+                  Start building an online community through social media
+                  channels, forums, and outreach efforts.
+                </li>
+                <li>
+                  Engage with potential users to generate interest and gather
+                  feedback on desired features.
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div
@@ -46,11 +89,52 @@ function RoadMap() {
             </p>
             <FaCircleChevronDown className="text-white text-[30px]" />
           </div>
-          <div className="bg-white m-[10px_20px]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
-            assumenda nostrum nam nihil at ullam quos veniam consectetur iusto
-            corrupti, a ipsa laborum sit fugit eligendi placeat, quia error
-            eveniet.
+          <div className="bg-white m-[10px_20px] p-[10px] rounded-[6px] flex flex-col space-y-[10px]">
+            <div>
+              <p className="font-syne text-[18px] font-[600] text-[#081428]">
+                1. Platform Launch
+              </p>
+              <ul className="font-syne list-disc px-[20px]">
+                <li>
+                  Officially launch AlphaXBot to the public with a targeted
+                  marketing campaign.
+                </li>
+                <li>
+                  Highlight the platform&apos;s unique features, benefits, and
+                  competitive advantages.
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p className="font-syne text-[18px] font-[600] text-[#081428]">
+                2. User Acquisition
+              </p>
+              <ul className="font-syne list-disc px-[20px]">
+                <li>
+                  Implement user acquisition strategies to attract traders from
+                  various backgrounds and experience levels.
+                </li>
+                <li>
+                  Offer promotional deals or incentives to encourage early
+                  adoption and user engagement.
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p className="font-syne text-[18px] font-[600] text-[#081428]">
+                3. User Support and Feedback
+              </p>
+              <ul className="font-syne list-disc px-[20px]">
+                <li>
+                  Provide responsive customer support to address user inquiries,
+                  issues, and feedback.
+                </li>
+                <li>
+                  Continuously gather user feedback to identify areas for
+                  improvement and feature enhancements.
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div
@@ -67,17 +151,59 @@ function RoadMap() {
             </p>
             <FaCircleChevronDown className="text-white text-[30px]" />
           </div>
-          <div className="bg-white m-[10px_20px]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
-            assumenda nostrum nam nihil at ullam quos veniam consectetur iusto
-            corrupti, a ipsa laborum sit fugit eligendi placeat, quia error
-            eveniet.
+          <div className="bg-white m-[10px_20px] p-[10px] rounded-[6px] flex flex-col space-y-[10px]">
+            <div>
+              <p className="font-syne text-[18px] font-[600] text-[#081428]">
+                1. Feature Expansion
+              </p>
+              <ul className="font-syne list-disc px-[20px]">
+                <li>
+                  Expand the platform&apos;s feature set based on user feedback
+                  and market trends.
+                </li>
+                <li>
+                  Introduce new functionalities such as advanced trading
+                  options, portfolio management tools, and analytics.
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p className="font-syne text-[18px] font-[600] text-[#081428]">
+                2. Community Growth
+              </p>
+              <ul className="font-syne list-disc px-[20px]">
+                <li>
+                  Scale up community-building efforts to grow the user base and
+                  foster a vibrant trading community.
+                </li>
+                <li>
+                  Organize webinars, workshops, and events to educate users and
+                  facilitate knowledge sharing
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p className="font-syne text-[18px] font-[600] text-[#081428]">
+                3. Partnerships and Integrations
+              </p>
+              <ul className="font-syne list-disc px-[20px]">
+                <li>
+                  Forge partnerships with exchanges, DeFi protocols, and other
+                  blockchain projects to enhance interoperability and
+                  functionality.
+                </li>
+                <li>
+                  Explore integration opportunities with third-party tools and
+                  services to provide added value to users.
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div
           className={`${
             step === 4 ? "h-fit " : "h-[70px]"
-          } rd3 rounded-[10px] lg:rounded-[12px] transition-all overflow-hidden`}
+          } rd4 rounded-[10px] lg:rounded-[12px] transition-all overflow-hidden`}
         >
           <div
             onClick={() => setStep((prev) => (prev === 4 ? 0 : 4))}
@@ -88,11 +214,67 @@ function RoadMap() {
             </p>
             <FaCircleChevronDown className="text-white text-[30px]" />
           </div>
-          <div className="bg-white m-[10px_20px]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
-            assumenda nostrum nam nihil at ullam quos veniam consectetur iusto
-            corrupti, a ipsa laborum sit fugit eligendi placeat, quia error
-            eveniet.
+          <div className="bg-white m-[10px_20px] p-[10px] rounded-[6px] flex flex-col space-y-[10px]">
+            <div>
+              <p className="font-syne text-[18px] font-[600] text-[#081428]">
+                1. Scalability
+              </p>
+              <ul className="font-syne list-disc px-[20px]">
+                <li>
+                  Continuously optimize the platform&apos;s infrastructure to
+                  handle increasing user traffic and activity.
+                </li>
+                <li>
+                  Monitor performance metrics and implement scalability
+                  solutions as needed.
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p className="font-syne text-[18px] font-[600] text-[#081428]">
+                2. Monetization Strategies
+              </p>
+              <ul className="font-syne list-disc px-[20px]">
+                <li>
+                  Implement monetization strategies such as subscription plans,
+                  premium features, and referral programs.
+                </li>
+                <li>
+                  Explore additional revenue streams such as data licensing and
+                  sponsored content.
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p className="font-syne text-[18px] font-[600] text-[#081428]">
+                3. Regulatory Compliance
+              </p>
+              <ul className="font-syne list-disc px-[20px]">
+                <li>
+                  Stay updated on regulatory developments and ensure compliance
+                  with relevant laws and regulations.
+                </li>
+                <li>
+                  Implement robust security and data protection measures to
+                  safeguard user assets and privacy.
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p className="font-syne text-[18px] font-[600] text-[#081428]">
+                4. Continuous Improvement
+              </p>
+              <ul className="font-syne list-disc px-[20px]">
+                <li>
+                  Prioritize ongoing development, optimization, and innovation
+                  to maintain AlphaXBot&apos;s competitive edge in the market.
+                </li>
+                <li>
+                  Solicit user feedback and conduct market research to inform
+                  product roadmap decisions and future iterations.
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
